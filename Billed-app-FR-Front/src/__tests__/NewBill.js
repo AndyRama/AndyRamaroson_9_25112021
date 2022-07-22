9045d42a17acdaba0304ef0a9ad4d6e5dfa58d66
AndyRama <andyramaroson@gmail.com>
2022-07-22 15:05:18 +0200

    [ehancement] code for test unit NewBill import

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -5,26 +5,35 @@
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-// Import localStorageMock
-// Import Route
-// Import UserEvent
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import mockStore from "../__mocks__/store"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import userEvent from "@testing-library/user-event";
 
-//mockStore
+jest.mock("../app/Store", () => mockStore)
  
- //simule la connection sur la page Employee en parametrant le localStorage
- //affiche la page nouvelle note de frais
+beforeEach(() => {
+  //simule la connection sur la page Employee en parametrant le localStorage
+  Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+  window.localStorage.setItem('user', JSON.stringify({
+    type: 'Employee',
+    email: 'employee@test.tld'
+  }))
+  //affiche la page nouvelle note de frais
+  document.body.innerHTML = NewBillUI()
+})
 
  describe("Given I am connected as an employee", () => {
    describe("When I am on NewBill Page", () => {
      // recupération input file
-     //recupération instance de class NewBill
-     //eventListener handleChangeFile
-     //verifie si le fichier est bien un image
+     // recupération instance de class NewBill
+     // eventListener handleChangeFile
+     // verifie si le fichier est bien un image
 
-     //recupération input file
-     //recupération instance de class NewBill
-     //eventListener handleChangeFile
-     //verifie si le fichier est bien un image
+     // recupération input file
+     // recupération instance de class NewBill
+     // eventListener handleChangeFile
+     // verifie si le fichier est bien un image
      
      test("Then ...", () => {
        const html = NewBillUI()
