4002d2b46abcd5de74adf3b8f234effd6ddbcdd3
AndyRama <andyramaroson@gmail.com>
2022-07-22 11:07:27 +0200

    [ehancement] pseudo code for test unit NewBill

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -5,18 +5,31 @@
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+// Import localStorageMock
+// Import Route
+// Import UserEvent
 
+//mockStore
+ 
+ //simule la connection sur la page Employee en parametrant le localStorage
+ //affiche la page nouvelle note de frais
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    //envoyer un formulaire plein NewBill.handleSubmit
-    //justificatif doit etre conformepour l'extension image(png, jpeg, jpg)
-    //justificatif ne doit pas être au format pdf 
-    
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+ describe("Given I am connected as an employee", () => {
+   describe("When I am on NewBill Page", () => {
+     // recupération input file
+     //recupération instance de class NewBill
+     //eventListener handleChangeFile
+     //verifie si le fichier est bien un image
+
+     //recupération input file
+     //recupération instance de class NewBill
+     //eventListener handleChangeFile
+     //verifie si le fichier est bien un image
+     
+     test("Then ...", () => {
+       const html = NewBillUI()
+       document.body.innerHTML = html
+       //to-do write assertion
+     })
+   })
+ })
