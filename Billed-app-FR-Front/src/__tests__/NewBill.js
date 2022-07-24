20c7a6804dca89a46a0ea0dde52283c0ef34f98e
AndyRama <andyramaroson@gmail.com>
2022-07-24 13:32:17 +0200

    [fix] code for test unit NewBill type image work

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -2,42 +2,50 @@
  * @jest-environment jsdom
  */
 
- import { screen, fireEvent, waitFor } from "@testing-library/dom"
- import userEvent from "@testing-library/user-event";
- import NewBillUI from "../views/NewBillUI.js"
- import NewBill from "../containers/NewBill.js"
- import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
- import { localStorageMock } from "../__mocks__/localStorage.js"
- import mockStore from "../__mocks__/store"
+import { screen, fireEvent, waitFor } from "@testing-library/dom"
+import NewBillUI from "../views/NewBillUI.js"
+import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import mockStore from "../__mocks__/store"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import userEvent from "@testing-library/user-event";
 //  import router from "../app/Router"
- 
- jest.mock("../app/Store", () => mockStore)
- 
- beforeEach(() => {
-   //simule la connection sur la page Employee en parametrant le localStorage
-   Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-   window.localStorage.setItem('user', JSON.stringify({
-     type: 'Employee',
-     email: 'employee@test.tld'
-   }))
-   //affiche la page nouvelle note de frais
-   document.body.innerHTML = NewBillUI()
- })
 
- // Model for test d'intégration GET NewBills
-describe('NewBill Unit Test Suites', () => {
+jest.mock("../app/Store", () => mockStore)
 
+beforeEach(() => {
+  //On simule la connection sur la page Employee en parametrant le localStorage
+  Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+  window.localStorage.setItem('user', JSON.stringify({
+    type: 'Employee',
+    email: 'employee@test.tld'
+  }))
+  // Afficher la page nouvelle note de frais
+  document.body.innerHTML = NewBillUI()
+})
+
+describe("NewBill Unit test suites", () => {
   describe("Given I am connected as an employee", () => {
-    describe("When I am on NewBill Page", () => {
-      describe("When I try to load file", () => {
-        test("Then file should be an image", () => {
-
-          // recupération input file
+    describe("When I am on NewBill page", () => {
+      describe("When I try to load a picture", () => {
+        test("Then file sould be an picture", () => {
+          // Récupération input file
           const newFile = screen.getByTestId('file')
-          // recupération instance de class NewBill
+          // Récupération de nouvelle instance de NewBill
           const onNavigate = (pathname) => document.body.innerHTML = ROUTES({ pathname })
           const newBillEmulation = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
+          const handleChangeFile = jest.fn((e) => newBillEmulation.handleChangeFile(e))
+          // addEventListener handleChangeFile
+          newFile.addEventListener("change", handleChangeFile)
+          userEvent.click(newFile)
+          //  Vérifié si le fichier est bien une image
+          fireEvent.change(newFile, {
+            target: {
+              files: [new File(['(⌐□_□)'], 'chucknorris.png', { type: 'image/png'})],
+            },
+          })
           expect(newFile.files[0].type).toMatch(/(image\/jpg)|(image\/jpeg)|(image\/png)/gm)
+          // window.alert = jsdomAlert;  // restore the jsdom alert
         })
       })
     })
